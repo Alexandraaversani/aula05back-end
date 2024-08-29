1805a88e83e45ff9299471453a2d80081aac04e9
Alexandra Aversani <alexandraaversani.tds2.senai@gmail.com>
2024-08-29 10:52:56 -0300

    feat: add some requests

--- src/server.js
+++ src/server.js
@@ -8,6 +8,47 @@ const serverPort = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
 
+const emocoes = [
+    {
+        id: 1,
+        nome: "Nojinho",
+        cor: "Verde"
+    },
+
+    {
+        id: 2,
+        nome: "Alegria",
+        cor: "Amarelo"
+    },
+
+    {
+        id: 3,
+        nome: "Raiva",
+        cor: "Vermelho"
+    }
+
+]
+
+const personagens = [
+    {
+        id: 100,
+        nome: "Totoro",
+        vivo: true
+    },
+
+    {
+        id: 101,
+        nome: "Rapunzel",
+        vivo: "true"
+    },
+
+    {
+        id: 102,
+        nome: "Blue",
+        vivo: "true"
+    }
+]
+
 app.get("/", (req, res) => {
     return res.status(200).send({ message: "Hello, world!"})
 
@@ -15,6 +56,16 @@ app.get("/", (req, res) => {
 
 app.get("/2tds2", (req, res) => {
     return res.status(200).send({ message: "Hello, world!"})
+
+})
+
+app.get("/emocoes", (req, res) => {
+    return res.status(200).send( emocoes )
+
+})
+
+app.get("/personagens", (req, res) => {
+    return res.status(200).send( personagens )
 
 })
 
